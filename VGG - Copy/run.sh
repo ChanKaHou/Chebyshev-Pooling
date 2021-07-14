a1301844a710b95a81fd8098a83461595bcab802
Ka-Hou Chan <38522240+ChanKaHou@users.noreply.github.com>
2021-07-14 11:21:57 +0800

    Add files via upload

---
+++ VGG - Copy/run.sh
@@ -0,0 +1,8 @@
+python3 VGG_ChebyPooling-CIFAR10.py
+python3 VGG_ChebyPooling-CIFAR10.py
+python3 VGG_ChebyPooling-CIFAR10.py
+python3 VGG_ChebyPooling-CIFAR10.py
+python3 VGG_ChebyPooling-CIFAR10.py
+python3 VGG_ChebyPooling-CIFAR10.py
+python3 VGG_ChebyPooling-CIFAR10.py
+python3 VGG_ChebyPooling-CIFAR10.py
